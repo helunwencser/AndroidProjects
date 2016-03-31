5a51ea53b5b561541ac7941f6228215f160cea9f
helunwencser <lunwenh@andrew.cmu.edu>
2016-03-30 17:34:16 -0700

    add audio and vedio player

--- Artist/app/src/main/java/edu/cmu/lunwenh/artist/MainActivity.java
+++ Artist/app/src/main/java/edu/cmu/lunwenh/artist/MainActivity.java
@@ -1,22 +1,43 @@
 package edu.cmu.lunwenh.artist;
 
 import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.ScrollView;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.VideoView;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] audios;
+    private String[] vedios;
 
     int[] cargallery = {
             R.drawable.image1,
@@ -111,15 +132,165 @@ public class MainActivity extends AppCompatActivity {
 
         setPersonalInfo();
 
-        try {
-            AssetFileDescriptor assetFileDescriptor = this.getAssets().openFd("audio1.mp3");
-            MediaPlayer mediaPlayer = new MediaPlayer();
-            mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(), assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength());
-            mediaPlayer.prepare();
-            mediaPlayer.start();
+        AssetManager assetManager = this.getAssets();
+        try{
+            audios = assetManager.list("audio");
+            Arrays.sort(audios);
+            vedios = assetManager.list("vedio");
+            Arrays.sort(vedios);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        addItemsToListView(audios, "audio");
+        addItemsToListView(vedios, "vedio");
+
+        addListenerOnAudioListView();
+        addListenerOnVedioListView();
+    }
+
+    /**
+     * add items to ListView
+     * @param   items
+     *          the name of items
+     *
+     * @param   type
+     *          type of list, possible values are: audio, vedio
+     * */
+    private void addItemsToListView(String[] items, String type) {
+        ListView listView = null;
+        if(type.equals("audio")) {
+            listView = (ListView)this.findViewById(R.id.listViewOfAudio);
+        } else if(type.equals("vedio")) {
+            listView = (ListView)this.findViewById(R.id.listViewOfVedio);
+        }
+        if(linearLayout != null) {
+            List<String> list = new ArrayList<String>();
+            list.addAll(Arrays.asList(items));
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.simplerow, list);
+            listView.setAdapter(adapter);
+        }
+    }
+
+    /**
+     * add item click listener on audio list view
+     * */
+    private void addListenerOnAudioListView() {
+        final ListView listViewAudio = (ListView)this.findViewById(R.id.listViewOfAudio);
+        listViewAudio.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                LayoutInflater layoutInflater = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                final View popupView = layoutInflater.inflate(R.layout.audio_control, null);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+
+                Button pauseButton = (Button)popupView.findViewById(R.id.pauseAudio);
+                Button startButton = (Button)popupView.findViewById(R.id.startAudio);
+                Button stopButton = (Button)popupView.findViewById(R.id.stopAudio);
+
+                VideoView videoView = (VideoView)findViewById(R.id.videoView);
+
+                String item = (String)listViewAudio.getItemAtPosition(position);
+                final MediaPlayer mediaPlayer;
+                try{
+                    AssetFileDescriptor assetFileDescriptor = getAssets().openFd(item);
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(
+                            assetFileDescriptor.getFileDescriptor(),
+                            assetFileDescriptor.getStartOffset(),
+                            assetFileDescriptor.getLength()
+                    );
+                    mediaPlayer.setDisplay(videoView.getHolder());
+
+                    mediaPlayer.prepare();
+
+                    pauseButton.setOnClickListener(new Button.OnClickListener() {
+
+                        public void onClick(View arg0) {
+                            mediaPlayer.pause();
+                        }
+                    });
+
+                    startButton.setOnClickListener(new Button.OnClickListener() {
+
+                        public void onClick(View arg0) {
+                            mediaPlayer.start();
+                        }
+                    });
+
+                    stopButton.setOnClickListener(new Button.OnClickListener() {
+
+                        public void onClick(View arg0) {
+                            mediaPlayer.stop();
+                            popupWindow.dismiss();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * add listener on vedio list view
+     * */
+    private void addListenerOnVedioListView() {
+        final ListView listViewVedio = (ListView)this.findViewById(R.id.listViewOfVedio);
+
+        listViewVedio.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                LayoutInflater layoutInflater = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                final View popupView = layoutInflater.inflate(R.layout.vedio_control, null);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+
+                Button pauseButton = (Button)popupView.findViewById(R.id.pauseVedio);
+                Button startButton = (Button)popupView.findViewById(R.id.startVedio);
+                Button stopButton = (Button)popupView.findViewById(R.id.stopVedio);
+
+                String item = (String)listViewVedio.getItemAtPosition(position);
+                final MediaPlayer mediaPlayer;
+                try{
+                    AssetFileDescriptor assetFileDescriptor = getAssets().openFd(item);
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor());
+                    mediaPlayer.prepare();
+
+                    pauseButton.setOnClickListener(new Button.OnClickListener() {
+
+                        public void onClick(View arg0) {
+                            mediaPlayer.pause();
+                        }
+                    });
+
+                    startButton.setOnClickListener(new Button.OnClickListener() {
+
+                        public void onClick(View arg0) {
+                            mediaPlayer.start();
+                        }
+                    });
+
+                    stopButton.setOnClickListener(new Button.OnClickListener() {
+
+                        public void onClick(View arg0) {
+                            mediaPlayer.stop();
+                            popupWindow.dismiss();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
